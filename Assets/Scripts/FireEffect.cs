ec3a159057f7615ee1ecdeddcf538fe808f9041b
MindYume <youwatchdream@gmail.com>
2022-10-06 21:03:44 -0600

    Sound fix

--- Assets/Scripts/FireEffect.cs
+++ Assets/Scripts/FireEffect.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class FireEffect : MonoBehaviour
 {
+    void Start()
+    {
+        GetComponent<AudioSource>().time = Random.Range(0f, GetComponent<AudioSource>().clip.length);
+    }
+
     void Update()
     {
         transform.rotation = Quaternion.Euler(new Vector3(-90f, 0f, 0f));
